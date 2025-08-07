b14e36700d5551ebb9778e8796599b636484bac4
slrmyapi <slrmyapi@slrmyapi.org>
2025-08-07 08:48:53 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 export default {
   async fetch(request, env) {
+    const searchParams = new URLSearchParams(request.url);
+    const prompt = searchParams.get("prompt");
+    if (!prompt) return Response.json({
+      message: "Missing param (prompt)",
+    })
     const inputs = {
-      prompt: "cyberpunk cat",
+      prompt,
     };
 
     const response = await env.AI.run(
